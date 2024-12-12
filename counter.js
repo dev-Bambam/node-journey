d622485f9303cf620f6de5b135bca3b86e074df8
dev-Bambam <ayogood18@gmail.com>
2024-12-12 14:29:18 +0100

    moving on

--- counter.js
+++ counter.js
@@ -1,13 +1,12 @@
-function makeCounter() {
-    let counter = 0;
-    return function () {
-        counter++;
-        console.log(counter);
-    }
+ function Person(name, age) {
+  this.name = name;  // `this` refers to the newly created object
+  this.age = age;
+  
+  // Arrow function
+  this.greet = () => {
+    console.log(`Hi, my name is ${this.name} and I'm ${this.age} years old.`);
+  };
 }
 
-var counter = makeCounter();
-counter();
-counter();
-counter(); 
-counter();
+const person2 = new Person('Bambam', 25);
+person2.greet();  // Output: "Hi, my name is Bambam and I'm 25 years old."

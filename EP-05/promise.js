8d72353c4f815057e0aafb50e1cf06abbf09c773
dev-Bambam <ayogood18@gmail.com>
2024-12-31 12:15:28 +0100

    ...

--- EP-05/promise.js
+++ EP-05/promise.js
@@ -1,16 +1,35 @@
-const myPromise = new Promise((resolve, reject) => {
-    // simulate async operation with setTimeout
-    setTimeout(() => {
-        const success = true;
-        success ? resolve('Operation successful') : reject('Operation failed')
-    }, 2000)
-});
-myPromise
-    .then(result => {
-        console.log(result);
-    })
-    .catch(error => {
-        console.log(error)
-    });
+// const myPromise = new Promise((resolve, reject) => {
+//     // simulate async operation with setTimeout
+//     setTimeout(() => {
+//         const success = true;
+//         success ? resolve('Operation successful') : reject('Operation failed')
+//     }, 2000)
+// });
+// myPromise
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     });
 
 console.log('my Promise tutorial')
+
+// building a mini project 
+
+const fetchData = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            fetch("https://jsonplaceholder.typicode.com/posts/1")
+                .then((response) => response.json())
+                .then((data) => resolve(data))
+                .catch((error) => reject(error));
+        }, 2000)
+    })
+};
+
+// consuming Promise 
+const dataFetch = fetchData();
+dataFetch
+    .then(data => console.log(`resulting responses: ${JSON.stringify(data, null, 5)}`))
+    .catch(error => console.log(`this error:[ ${error} ]occured`))

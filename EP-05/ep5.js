dff92212e0a72507f7ac8c048888b8d0c7172142
dev-Bambam <ayogood18@gmail.com>
2024-12-31 10:17:18 +0100

    moving on

--- EP-05/ep5.js
+++ EP-05/ep5.js
@@ -1,16 +1,9 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.question("please enter your name: ", (name) => {
-  console.log(`You entered ${name}`);
-  rl.close();
-});
-let timeout = 2000;
-setTimeout(() => {
-  rl.on("close", () => {
-    console.log("interface closed");
-    process.exit(0);
-  });
-}, timeout);
+const fs = require('fs');
+fs.readFile('./input.txt', 'utf-8', (error, data) => {
+    data ? toJSON(data) : console.log(error)
+    console.log(data)
+})
+console.log('reading file ...')
+function toJSON(data) {
+    const json = JSON.parse(data);
+}

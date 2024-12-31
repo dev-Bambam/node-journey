b40e74b95aae95a28d87fba4dbb6b948408a0886
dev-Bambam <ayogood18@gmail.com>
2024-12-31 13:16:45 +0100

    ...

--- EP-05/promise.js
+++ EP-05/promise.js
@@ -17,19 +17,45 @@ console.log('my Promise tutorial')
 
 // building a mini project 
 
-const fetchData = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            fetch("https://jsonplaceholder.typicode.com/posts/1")
-                .then((response) => response.json())
-                .then((data) => resolve(data))
-                .catch((error) => reject(error));
-        }, 2000)
-    })
-};
+// const fetchData = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             fetch("https://jsonplaceholder.typicode.com/posts/1")
+//                 .then((response) => response.json())
+//                 .then((data) => resolve(data))
+//                 .catch((error) => reject(error));
+//         }, 2000)
+//     })
+// };
 
-// consuming Promise 
-const dataFetch = fetchData();
-dataFetch
-    .then(data => console.log(`resulting responses: ${JSON.stringify(data, null, 5)}`))
-    .catch(error => console.log(`this error:[ ${error} ]occured`))
+// // consuming Promise 
+// const dataFetch = fetchData();
+// dataFetch
+//     .then(data => console.log(`resulting responses: ${JSON.stringify(data, null, 5)}`))
+//     .catch(error => console.log(`this error:[ ${error} ]occured`))
+
+// another mini tutorial for Promise.all()
+
+const fetchData = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(error => reject(error))
+    })
+}
+const fetchAllData = () => {
+    return Promise.all([
+        fetchData('https://jsonplaceholder.typicode.com/posts/1'),
+        fetchData('https://jsonplaceholder.typicode.com/users/1')
+    ])
+}
+const myData = fetchAllData()
+
+myData
+    .then(result => {
+        const [posts, user] = result;
+        console.log(`Posts: ${JSON.stringify(posts, null, 3)}`);
+        console.log(`User: ${JSON.stringify(user, null, 3)}`);
+    })
+.catch(error => console.log(error))

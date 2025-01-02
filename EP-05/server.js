a99ffe37e290625017e6d1b9547b6ca2a68c65f9
dev-Bambam <ayogood18@gmail.com>
2025-01-02 15:02:28 +0100

    created a simple server

--- EP-05/server.js
+++ EP-05/server.js
@@ -1,9 +1,28 @@
-import http from 'http'
+import http from 'http';
+import fs from 'fs';
 
+// reading in file 
+const landingPage = fs.readFileSync('./index.html', 'utf-8')
 // step 1: create server
-const server = http.createServer((req, resp) => {
-    resp.end('hello from server')
-    console.log('request recieved');
+const server = http.createServer((request, response) => {
+    let path = request.url;
+    path = path.toLocaleLowerCase();
+
+    // creating a simple route
+    if (path === '/' || path === '/home') {
+        response.writeHead(200, {
+            "content-type": 'text/html'
+        });
+        response.end(landingPage.replace('{{%%CONTENT%%}}', 'You are in the hompage'));
+        response.end(landingPage.replace('{{%%CONTENT%%}}', 'You are in the hompage'));
+    } else if (path === '/about') {
+        response.writeHead(200, {
+          "content-type": "text/html",
+        });
+        response.end(
+          landingPage.replace("{{%%CONTENT%%}}", "You are in the about page")
+        );
+    }
 });
 // step 2: listen to the server
 server.listen(8000, '127.0.0.1', () => {
